e4216f295eb56d8d659b9822641d11147aa2d698
RakeShrestha <orakeshstha7@gmail.com>
2025-01-31 06:58:30 +0545

    Initial Commit

---
+++ SwiftUIing Fitness/SwiftUIing_FitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIing_FitnessApp.swift
+//  SwiftUIing Fitness
+//
+//  Created by Rakesh Shrestha on 31/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIing_FitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
